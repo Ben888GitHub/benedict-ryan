5d000c199199e25bc27796925c068406f225ed20
Benedict <benedictryan80@gmail.com>
2022-12-06 15:10:36 +0700

    modify codes 5

--- components/Technologies/Skills.js
+++ components/Technologies/Skills.js
@@ -4,10 +4,11 @@ import {
 	SiFirebase,
 	SiGit,
 	SiRedux,
-	SiGraphql,
+	SiTailwindcss,
 	SiNetlify,
 	SiNextdotjs,
-	SiTypescript
+	SiTypescript,
+	SiBootstrap
 } from 'react-icons/si';
 import { RiFlutterFill } from 'react-icons/ri';
 import { FaNodeJs } from 'react-icons/fa';
@@ -92,36 +93,32 @@ export const Skills = [
 		Component: SiFirebase,
 		title: 'Firebase',
 		Description: () => (
-			<>I have used Firebase for auth, database & analytics in my apps.</>
-		)
-	},
-
-	{
-		slug: 'graphql',
-		Component: SiGraphql,
-		title: 'GraphQL',
-		Description: () => (
-			<>I use GraphQL along with Apollo Client for querying GraphQL API.</>
+			<>
+				I have used Firebase for auth, database, storage & analytics in my apps.
+			</>
 		)
 	},
 	{
-		slug: 'nodejs',
-		Component: FaNodeJs,
-		title: 'NodeJs',
+		slug: 'tailwindcss',
+		Component: SiTailwindcss,
+		title: 'TailwindCSS',
 		Description: () => (
 			<>
-				NodeJs is a backend language that I use for logic creation and setup of
-				API that requires the server to access
+				I utilize Tailwind to construct my apps with holistic features of UI and
+				Responsive Web Design.
 			</>
 		)
 	},
 
 	{
-		slug: 'git',
-		Component: SiGit,
-		title: 'Git',
+		slug: 'bootstrap',
+		Component: SiBootstrap,
+		title: 'Bootstrap',
 		Description: () => (
-			<>Git is a tool that I use every day. I use GitHub for pushing my code.</>
+			<>
+				Beside Tailwind, Bootstrap is the UI Framework that I normally use for
+				my basic prototype projects or POCs
+			</>
 		)
 	}
 ];
